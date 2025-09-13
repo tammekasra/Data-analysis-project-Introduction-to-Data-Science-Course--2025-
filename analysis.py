ac2e614f7e57ed41430f65844a9ee69ad098e7d5
tammekasra <Ranel.tammela@gmail.com>
2025-09-13 19:18:53 +0300

    Update analysis.py
    We did mean value analysis

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 
 '''
@@ -20,8 +21,24 @@ This just does the StressLevelDataset.csv
 
 
 plt.figure(figsize=(12, 8))
-sns.heatmap(stress_level_df.corr(), annot=True, fmt='.2f', cmap='coolwarm')
+sns.heatmap(stress_level_df.corr(), annot=True, cmap='coolwarm')
 plt.title('Heatmap - StressLevelDataset.csv')
+plt.tight_layout()
+plt.show()
+
+
+
+'''
+This makes the heatmap look prettier
+'''
+stress_corr = stress_level_df.corr()['stress_level']
+stress_corr_sorted = stress_corr.sort_values(ascending=False)
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x=stress_corr_sorted.values, y=stress_corr_sorted.index, palette='coolwarm')
+plt.title('Correlation of Factors with Stress Level')
+plt.xlabel('Correlation Coefficient')
+plt.ylabel('Factor')
 plt.tight_layout()
 plt.show()
 
@@ -34,3 +51,43 @@ This is for the Stress_DataSet.csv
 #plt.title('Heatmap - Stress_Dataset.csv')
 #plt.tight_layout()
 #plt.show()
+
+
+
+'''
+We can divide the data into two - low stress and high stress students and see
+how they correlate with sleep_quality, self_esteem,academic_performance,future_career_concerns
+if there are any signficant changes.
+'''
+
+'''
+Here we just take median and later split it to low stress (below median values) and high stress
+'''
+median_stress = stress_level_df['stress_level'].median()
+
+stress_level_df['stress_group'] = np.where(stress_level_df['stress_level'] > median_stress, 
+                                          'High Stress', 
+                                          'Low Stress')
+
+factors_to_compare = ['sleep_quality', 'self_esteem', 'academic_performance', 'future_career_concerns']
+
+comparison_df = stress_level_df.groupby('stress_group')[factors_to_compare].mean().T
+print(comparison_df)
+
+
+comparison_df.plot(kind='bar', figsize=(10, 6), color=['lightcoral', 'lightgreen'])
+plt.title('Average Factor Levels by Stress Group')
+plt.ylabel('Average Score')
+plt.legend(title='Stress Group')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
